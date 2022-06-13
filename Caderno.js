981106acbbfb627b379810d70903b9dfc1ff431c
Thomas Schreiner <iteano.thom@hotmail.com>
2022-06-13 15:09:07 -0300

    adicionando Arquivo JS

---
+++ Caderno.js
@@ -0,0 +1,11 @@
+// Variável
+let variável = "Variável que pode ser alterada a qualquer momento"
+const constante = "Define um variável que NÃO pode ser alterada"
+
+// Comandos
+console.log("Escreve na tela o valor que estiver AQUI")
+alert("Cria um Alerta que sobrepoe a página com o que estiver Aqui")
+
+{
+    "Este espaço se chama Escopo"
+}
